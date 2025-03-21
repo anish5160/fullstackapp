ae6a1426b6e90df35d643f5074b011719447cac7
Anesh Dwibedi <aneshdwibedi115@gmail.com>
2025-03-22 03:00:14 +0530

    Updated dependencies and fixed issues

--- djangoauthapi1/settings.py
+++ djangoauthapi1/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ['fullstackapp-9.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['fullstackapp-11.onrender.com', 'localhost', '127.0.0.1']
 
 
 
